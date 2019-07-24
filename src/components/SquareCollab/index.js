a92653c47df525ba2bd9007c4a977ebe0316e2b8
Anderson Garcia <andersongarciapinheiro@gmail.com>
2019-07-24 16:31:21 -0300

    challenge third day complete

--- src/components/SquareCollab/index.js
+++ src/components/SquareCollab/index.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-let content = true;
+class SquareCollab extends Component {
 
-const handleClick = (e) => {
-if(content) {
-    e.target.innerHTML = "o"
-    content = false;
-  } else {
-    e.target.innerHTML = "x"
-    content = true;
+  state = {content: "X"}
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       content: ""
+  //     }
+  // }
+  
+
+
+  handleClick = () => {
+    // const newState = {};
+    // if(this.state.content === "X") {
+    //   newState.content = "O";
+    // } else {
+    //   newState.content = "X";
+    // }
+    this.setState(
+      this.state.content === "X" ? 
+        {content: "O"} : 
+        {content: "X"}
+      );
+  };
+
+  render() {
+    return (
+        <button
+          onClick={this.handleClick}
+          className="square-collab">
+            {this.state.content}
+          
+          </button>
+    )
   }
 }
+// let content = true;
 
-const SquareCollab = () => (
+// const handleClick = (e) => {
+// if(content) {
+//     e.target.innerHTML = "o"
+//     content = false;
+//   } else {
+//     e.target.innerHTML = "x"
+//     content = true;
+//   }
+// }
 
-  <button
-      onClick={handleClick}
-      className="square-collab"></button>
+// const SquareCollab = () => (
 
-);
+//   <button
+//       onClick={handleClick}
+//       className="square-collab"></button>
+
+// );
 
 export default SquareCollab;
